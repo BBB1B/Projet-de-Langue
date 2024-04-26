82a15826fbb60423ced4e42dbcae7e8ba7d3df80
Nayel E <99287625+BBB1B@users.noreply.github.com>
2024-04-26 19:22:17 +0200

    ajout des pages europe, puissances et amérique du nord

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,29 @@
-function Affichern1(partie) //Ca devrait marcher
-{
-    var etat = document.getElementByClassName(partie).getElementById("fleche"); //proclème avec ClassName
-    var fleche = document.getElementByClassName(partie).getElementByTagName(button).getElementByTagName(img);
-    console.assert(etat.class)
-    if (etat.className == "droite")
-    {
-        etat.className == "bas";
-        fleche.style.transform = rotate("-90deg");
-    }
-    else
-    {
-        etat.className == "droite";
-        fleche.style.transform = rotate("90deg");
-    }
-}
+$(window).scroll(function() {
+  
+    // selectors
+    var $window = $(window),
+        $body = $('body'),
+        $panel = $('.panel');
+    
+    // Change 33% earlier than scroll position so colour is there when you arrive.
+    var scroll = $window.scrollTop() + ($window.height() / 3);
+   
+    $panel.each(function () {
+      var $this = $(this);
+      
+      // if position is within range of this panel.
+      // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+      // Remember we set the scroll to 33% earlier in scroll var.
+      if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+            
+        // Remove all classes on body with color-
+        $body.removeClass(function (index, css) {
+          return (css.match (/(^|\s)painting-\S+/g) || []).join(' ');
+        });
+         
+        // Add class of currently active div
+        $body.addClass('painting-' + $(this).data('painting'));
+      }
+    });    
+    
+  }).scroll();
